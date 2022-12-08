8b6a05a0768789960fb16461ee1d0f583eb493bd
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-12-08 10:55:05 +0000

    Logbook 4 Task 4 finished

---
+++ ContosoRazor/Pages/About.cshtml.cs
@@ -0,0 +1,34 @@
+using ContosoRazor.Data;
+using ContosoRazor.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoRazor.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AboutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Students { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Students = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
